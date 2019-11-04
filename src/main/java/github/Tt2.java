bbc678630cf87d34c0b3570865607918b7cac328
sakiran-sai <saikiran.k0441@gmail.com>
2019-11-04 21:47:33 +0530

    new tt2 class has been added into project

---
+++ src/main/java/github/Tt2.java
@@ -0,0 +1,13 @@
+package github;
+
+public class Tt2 {
+	
+	public void te(){
+		System.out.println("am tt2 main");
+	}
+	
+	public void ter(){
+		System.out.println("am add another method");
+	}
+
+}
